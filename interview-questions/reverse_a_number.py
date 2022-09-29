2f5ba5523f624ee5895b8341b77062b6e2aaa793
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-09-29 10:40:30 +0100

    Implements code to meet required functionalities

--- interview-questions/reverse_a_number.py
+++ interview-questions/reverse_a_number.py
@@ -1,1 +1,17 @@
-
+def reverse_number(n):
+    # convert int to string
+    number = str(n)
+    reverse_num = ''        
+    # reverse number
+    if len(number) == 1:
+        return n
+    else:
+        for char in number:
+            reverse_num = char + reverse_num
+        # check whether number is negative
+        if number[0] == '-':
+            reverse_num = reverse_num[:-1]
+            reverse_num = '-' + reverse_num
+        # convert string back to int
+        reverse_num = int(reverse_num)
+        return reverse_num
