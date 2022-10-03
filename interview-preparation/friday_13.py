a33fba8dc4e1c80627edd0641433aea623cf2858
CaterinaBi <catherinebonan@gmail.com>
2022-10-03 19:22:47 +0100

    Writes docstring

---
+++ interview-preparation/friday_13.py
@@ -0,0 +1,12 @@
+def friday_detector(month, year):
+    '''
+    A function that accepts to parameters, calculates whether a Friday 13 is in the month,
+    and return a bool
+
+    parameters:
+    month = int
+        two-digit integer indicating the month number
+    year = int
+        four-digit integer indicating the year number
+    '''
+    
