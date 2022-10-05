9359e0a03e3ab1a0b24bd562a3d173080c55832a
CaterinaBi <catherinebonan@gmail.com>
2022-10-05 21:38:32 +0100

    Completes function, implements test

--- interview-preparation/octal_numbers.py
+++ interview-preparation/octal_numbers.py
@@ -1,4 +1,10 @@
 # convert decimal numbers to octal numbers
 
 def decimal_to_octal(num):
-    print(f'{num} is oct{num} in octal.')
+    num = oct(num)
+    return num
+
+# test
+print(decimal_to_octal(7)) # should print 7
+print(decimal_to_octal(8)) # should print 10
+print(decimal_to_octal(16)) # should print 20
